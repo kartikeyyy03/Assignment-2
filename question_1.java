e0e3fa1af531697f76ff8925c264c70516f5e7a8
Kartikeya Kandpal <104270854+kartikeyyy03@users.noreply.github.com>
2023-06-01 22:30:48 +0530

    Add files via upload

---
+++ question_1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class question_1{
+
+public static void main(String[] args) {
+
+    
+
+
+    Scanner sc=new Scanner(System.in); //to read from the keyboard
+    System.out.println("Enter the Month number: ");
+    int A=sc.nextInt(); //To take input from the user(integer in this case)
+    
+    
+
+    if(A==1){System.out.println("January");}
+    else if(A==2){System.out.println("Feburary");}
+    else if(A==3){System.out.println("March");}
+    else if(A==4){System.out.println("April");}
+    else if(A==5){System.out.println("May");}
+    else if(A==6){System.out.println("June");}
+    else if(A==7){System.out.println("July");}
+    else if(A==8){System.out.println("August");}
+    else if(A==9){System.out.println("September");}
+    else if(A==10){System.out.println("October");}
+    else if(A==11){System.out.println("November");}
+    else if(A==12){System.out.println("December");}
+    else{System.out.println("Invalid Month");}
+
+
+    
+     
+
+    
+
+}
+
+
+    
+}
